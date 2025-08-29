8c99d40e14ab0dbd6ef95afde91a06b2f784d38f
DDEUGGP <ddeuggp@gmail.com>
2025-08-29 06:24:55 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
   // ...
+dependencies {
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
 
+    // Add the dependencies for the Crashlytics NDK and Analytics libraries
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-crashlytics-ndk")
+    implementation("com.google.firebase:firebase-analytics")
+}
   // Add the dependency for the Google services Gradle plugin
   id("com.google.gms.google-services") version "4.4.3" apply false
 
