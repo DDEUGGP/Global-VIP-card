a39278543ff7eea8d85cb0707918d45e11d708ef
DDEUGGP <ddeuggp@gmail.com>
2025-08-29 06:22:42 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,7 @@
-id("com.google.gms.google-services") version "4.4.3" apply false
+plugins {
+  // ...
+
+  // Add the dependency for the Google services Gradle plugin
+  id("com.google.gms.google-services") version "4.4.3" apply false
+
+}
